f1eb3a78982c7879d7c68eba680dcd5900fcc293
Marija Kukic <marijakukice2@gmail.com>
2018-04-08 22:25:45 +0200

    Tacka 2.7 Profil korisnika, lista bioskopa, lista pozorista.

--- src/app/home-page-for-user/home-page-for-user.component.ts
+++ src/app/home-page-for-user/home-page-for-user.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { RegistrationServiceService } from '../registration-service.service';
+import { TeatarServiceService } from '../teatar-service.service';
+import { MatTableDataSource, MatPaginator } from '@angular/material';
 
 @Component({
   selector: 'app-home-page-for-user',
@@ -7,9 +10,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageForUserComponent implements OnInit {
 
-  constructor() { }
+  korisnik: any;
+
+  displayedColumns = ['id', 'naziv', 'adresa', 'promotivniOpis'];
+  elementData: Bioskop[];
+  dataSource = new MatTableDataSource<Bioskop>(ELEMENT_DATA);
+  pozoriste:any;
+  bioskop:any;
+  dataSource2 = new MatTableDataSource<Bioskop>(ELEMENT_DATA);
+
+  constructor(public registrationService:RegistrationServiceService,
+              private teatarService:TeatarServiceService) { }
 
   ngOnInit() {
+
+    this.korisnik = this.registrationService.user;
+
+
+    this.teatarService.listaBioskopa()
+      .subscribe(data=>{
+        this.bioskop = data;
+        console.log(this.bioskop);
+        console.log(data.promotivniOpis) 
+        console.log("Ispisi nesto");
+
+        this.dataSource = new MatTableDataSource<Bioskop>(this.bioskop);
+       
+    })
+
+    this.teatarService.listaPozorista()
+    .subscribe(data=>{
+      this.pozoriste = data;
+      console.log(this.pozoriste);
+      console.log("Ispisi nesto");
+
+      this.dataSource2 = new MatTableDataSource<Bioskop>(this.pozoriste);
+     
+  })
   }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+}
+
+
+const ELEMENT_DATA: Bioskop[] = [
+  {id: '1', naziv: '', adresa:'', promotivniOpis:''}
+];
+  
+export interface Bioskop {
+  id: string;
+  naziv: string;
+  adresa: string;
+  promotivniOpis: string;
 }
